4ae430b18ab6a4c683a6ad3579c3ffa3bc2c048e
dciancu <daniel@iancu.io>
2024-06-14 23:24:47 +0300

    Fixed publish script version manifest

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ cd "$SCRIPT_DIR"
 image_name="${DOCKER_IMAGE:-dciancu/mdns-monitor}"
 
 function pushManifest() {
-    docker manifest create "$1" "${1}-arm" "${1}-x86"
+    docker manifest create "$1" "${2:-$1}-arm" "${2:-$1}-x86"
     docker manifest push "$1"
 }
 
@@ -17,8 +17,9 @@ echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin
 if [[ -n "${CIRCLE_BRANCH+x}" ]] && [[ "$CIRCLE_BRANCH" == 'test' ]]; then
     pushManifest "${image_name}:test"
 else
-    version="$(tr -d '\n' < VERSION.txt)"
     pushManifest "${image_name}:latest"
-    pushManifest "${image_name}:${version}"
+
+    version="$(tr -d '\n' < VERSION.txt)"
+    pushManifest "${image_name}:${version}" "${image_name}:latest"
 fi
 
