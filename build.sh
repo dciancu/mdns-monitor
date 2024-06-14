ce986d71fc5ae95fa39a236f3d2e6854c23a3fdc
dciancu <daniel@iancu.io>
2024-06-14 18:40:19 +0300

    Added architecture image suffix to build script

--- build.sh
+++ build.sh
@@ -6,16 +6,15 @@ SCRIPT_DIR="$(dirname "$0")"
 cd "$SCRIPT_DIR"
 
 image_name="${DOCKER_IMAGE:-dciancu/mdns-monitor}"
+image_suffix="-$(dpkg --print-architecture | tr -d '\n')"
 
 if [[ -n "${CIRCLE_BRANCH+x}" ]] && [[ "$CIRCLE_BRANCH" == 'test' ]]; then
-    docker build -t "${image_name}:test" --pull .
+    docker build -t "${image_name}:test${image_suffix}" --pull .
 else
     if [[ -n "${CIRCLE_BRANCH+x}" ]] && [[ "$CIRCLE_BRANCH" == 'build' ]]; then
         docker images | grep "$image_name" | tr -s ' ' | cut -d ' ' -f 2 \
             | xargs -I {} docker rmi "${image_name}:{}" || true
     fi
 
-    version="$(tr -d '\n' < VERSION.txt)"
-    docker build -t "$image_name" --pull .
-    docker tag "$image_name" "${image_name}:${version}"
+    docker build -t "${image_name}:latest${image_suffix}" --pull .
 fi
