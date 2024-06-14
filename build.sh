32c3a44a7bfa6d1007f456682afc3e145910dd3d
dciancu <daniel@iancu.io>
2024-06-14 22:50:01 +0300

    Fix circleci multi-arch build

--- build.sh
+++ build.sh
@@ -6,15 +6,20 @@ SCRIPT_DIR="$(dirname "$0")"
 cd "$SCRIPT_DIR"
 
 image_name="${DOCKER_IMAGE:-dciancu/mdns-monitor}"
-image_suffix="-$(dpkg --print-architecture | tr -d '\n')"
+image_arch="${BUILD_ARCH:-$(arch | tr -d '\n')}"
 
 if [[ -n "${CIRCLE_BRANCH+x}" ]] && [[ "$CIRCLE_BRANCH" == 'test' ]]; then
-    docker build -t "${image_name}:test${image_suffix}" --pull .
+    image_tag="${image_name}:test-${image_arch}"
 else
     if [[ -n "${CIRCLE_BRANCH+x}" ]] && [[ "$CIRCLE_BRANCH" == 'build' ]]; then
         docker images | grep "$image_name" | tr -s ' ' | cut -d ' ' -f 2 \
             | xargs -I {} docker rmi "${image_name}:{}" || true
     fi
+    image_tag="${image_name}:latest-${image_arch}"
+fi
 
-    docker build -t "${image_name}:latest${image_suffix}" --pull .
+docker build -t "$image_tag" --pull .
+
+if [[ -n "${CIRCLE_BRANCH+x}" ]]; then
+    docker push "$image_tag"
 fi
--- publish.sh
+++ publish.sh
@@ -9,9 +9,18 @@ echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 image_name="${DOCKER_IMAGE:-dciancu/mdns-monitor}"
 
+function pushManifest() {
+    docker manifest create "$1" \
+        --ammend "${1}-arm" \
+        --ammend "${1}-x86"
+    docker manifest push "$1"
+}
+
 if [[ -n "${CIRCLE_BRANCH+x}" ]] && [[ "$CIRCLE_BRANCH" == 'test' ]]; then
-    docker push "${image_name}:test"
+    pushManifest "${image_name}:test"
 else
-    docker rmi "${image_name}:test" 2>/dev/null || true
-    docker push --all-tags "$image_name"
+    version="$(tr -d '\n' < VERSION.txt)"
+    pushManifest "${image_name}:latest"
+    pushManifest "${image_name}:${version}"
 fi
+
